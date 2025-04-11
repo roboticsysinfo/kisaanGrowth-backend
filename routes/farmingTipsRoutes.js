b1c4928daef3e8cfdbcfc1d553f7e4c171d15607
roboticsysinfo <roboticsysinfo@gmail.com>
2025-04-11 14:35:30 +0530

    farmgin tips route

--- routes/farmingTipsRoutes.js
+++ routes/farmingTipsRoutes.js
@@ -7,11 +7,14 @@ const {authorize} = require("../middlewares/authMiddleware")
 // Create a tip
 router.post('/farming-tip/add', authorize(["admin"]), FarmingTipsController.createTip);
 
+
 // Get all tips
 router.get('/farming-tips/get', authorize(["farmer", "admin"]), FarmingTipsController.getTips);
 
+
 // Update a tip
 router.put('/farming-tip/update/:id', authorize(["admin"]), FarmingTipsController.updateTip);
+
 
 // Delete a tip
 router.delete('/farming-tip/delete/:id', authorize(["admin"]), FarmingTipsController.deleteTip);
