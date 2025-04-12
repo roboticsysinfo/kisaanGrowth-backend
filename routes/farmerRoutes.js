ff884ce75d078604d9cdc31c5db36b21ed46a9fe
roboticsysinfo <roboticsysinfo@gmail.com>
2025-04-12 10:00:41 +0530

    farmer routes auth

--- routes/farmerRoutes.js
+++ routes/farmerRoutes.js
@@ -26,7 +26,7 @@ router.post('/send-otp-to-farmer', sendOTPToFarmer);
 router.post("/farmer-login-otp-verify", farmerLoginWithOTP);
 
 
-router.get('/farmer/get/:farmerId', authorize(["farmer"]), getFarmerById);
+router.get('/farmer/get/:farmerId', authorize(["farmer", "admin"]), getFarmerById);
 
 
 router.get('/farmer/update/:farmerId', authorize(["farmer"]), updateFarmerById);
