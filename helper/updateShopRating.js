4afa2dab59ac011e19ba1adf978107c20d75cdcd
roboticsysinfo <roboticsysinfo@gmail.com>
2025-04-23 13:15:38 +0530

    review controller again update

--- helper/updateShopRating.js
+++ helper/updateShopRating.js
@@ -1,26 +1,31 @@
-const mongoose = require('mongoose');
 const Review = require('../models/Review');
+const mongoose = require('mongoose');
 const Shop = require('../models/Shop');
 
-
 const updateShopRating = async (shopId) => {
-  const stats = await Review.aggregate([
-    { $match: { shop_id: new mongoose.Types.ObjectId(shopId) } },
-    {
-      $group: {
-        _id: '$shop_id',
-        averageRating: { $avg: '$rating' },
-        totalReviews: { $sum: 1 }
+  
+  try {
+    const stats = await Review.aggregate([
+      { $match: { shop_id: new mongoose.Types.ObjectId(shopId) } },
+      {
+        $group: {
+          _id: '$shop_id',
+          averageRating: { $avg: '$rating' },
+          totalReviews: { $sum: 1 },
+        }
       }
-    }
-  ]);
+    ]);
 
-  const { averageRating = 0, totalReviews = 0 } = stats[0] || {};
+    const { averageRating = 0, totalReviews = 0 } = stats[0] || {};
 
-  await Shop.findByIdAndUpdate(shopId, {
-    averageRating: parseFloat(averageRating.toFixed(1)),
-    totalReviews
-  });
+    await Shop.findByIdAndUpdate(shopId, {
+      averageRating: parseFloat(averageRating.toFixed(1)),
+      totalReviews,
+    });
+  } catch (error) {
+    console.error("Error in updateShopRating:", error.message);
+    throw new Error("Error updating shop rating");
+  }
 };
 
-module.exports={ updateShopRating }
+module.exports = { updateShopRating };
