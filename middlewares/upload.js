99e23e557eecc0e06d10ebdb65d2599c9110c9be
roboticsysinfo <roboticsysinfo@gmail.com>
2025-05-02 14:44:19 +0530

    upload multer webp

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage: storage,
   fileFilter: (req, file, cb) => {
-    const filetypes = /jpeg|jpg|png|gif/webp;
+    const filetypes = /jpeg|jpg|png|gif|webp/
     const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
     const mimetype = filetypes.test(file.mimetype);
     if (extname && mimetype) {
