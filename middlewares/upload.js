561ca35a7e68c1cd8877ef4c43cb60ac3924f325
roboticsysinfo <roboticsysinfo@gmail.com>
2025-05-08 17:58:29 +0530

    redeem update function

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,27 +1,20 @@
-
-// Configure multer storage
+// multer-config.js
 const multer = require('multer');
 const path = require('path');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // specify the folder to store images
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname)); // create unique file name
-  }
-});
+const storage = multer.memoryStorage(); // Use memory storage instead of disk
 
 const upload = multer({
   storage: storage,
   fileFilter: (req, file, cb) => {
-    const filetypes = /jpeg|jpg|png|gif|webp/
+    const filetypes = /jpeg|jpg|png|gif|webp/;
     const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
     const mimetype = filetypes.test(file.mimetype);
+
     if (extname && mimetype) {
-      return cb(null, true);
+      cb(null, true);
     } else {
-      cb(new Error('Invalid file type. Only JPEG, PNG, and GIF are allowed.'));
+      cb(new Error('Invalid file type. Only JPEG, PNG, GIF, and WEBP are allowed.'));
     }
   }
 });
