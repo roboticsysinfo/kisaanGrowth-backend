f9a62f6318df39e4010a94bf0dc1be923ea0ff43
roboticsysinfo <roboticsysinfo@gmail.com>
2025-05-10 09:50:49 +0530

    customer redeem product model

--- models/CustomerRedeemProduct.js
+++ models/CustomerRedeemProduct.js
@@ -1,21 +1,25 @@
+const { default: mongoose } = require("mongoose");
+
 const customerRedeemProductSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  requiredPoints: {
-    type: Number,
-    required: true
-  },
-  rc_product_img: {
-    type: String,
-  },
-  price_value: {
-    type: Number,
-    required: true
-  }
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    requiredPoints: {
+        type: Number,
+        required: true
+    },
+    rc_product_img: {
+        type: String,
+    },
+    price_value: {
+        type: Number,
+        required: true
+    }
 }, { timestamps: true });
+
+module.exports = mongoose.model('CustomerRedeemProduct', customerRedeemProductSchema);
