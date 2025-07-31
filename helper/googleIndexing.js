e270eecbd8d21fcfefb4e1f5465c29909c0480b8
roboticsysinfo <roboticsysinfo@gmail.com>
2025-07-31 11:37:39 +0530

    updat file

--- helper/googleIndexing.js
+++ helper/googleIndexing.js
@@ -4,7 +4,7 @@ const path = require("path");
 async function indexURL(url) {
   try {
     const auth = new google.auth.GoogleAuth({
-      keyFile: path.join(__dirname, "kissan-growth-1746440205057-bab3df956012.json"), // JSON key file ka path
+      keyFile: path.join(__dirname, "kissan-growth-service-account"), // JSON key file ka path
       scopes: ["https://www.googleapis.com/auth/indexing"]
     });
 
